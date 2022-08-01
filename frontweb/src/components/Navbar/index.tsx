1766f08e1bd26213d2cf47c8c634973bd1ebaec7
NormandoJefferson <normandojefferson@hotmail.com>
2022-08-01 17:09:55 -0300

    Components, routes

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,14 +1,17 @@
-import './styles.css'
-import 'bootstrap/js/src/collapse.js'
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
-      <div className='container-fluid'>
-        <a href="link" className="nav-logo-text">
+      <div className="container-fluid">
+        {' '}
+        {/* previne quebra de linha entre logo e itens */}
+        <Link to="/" className="nav-logo-text">
           <h4>DS Catalog</h4>
-        </a>
-
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,23 +23,28 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-
         <div className="collapse navbar-collapse" id="dscatalog-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className='active'>HOME</a>
+              <NavLink to="/" activeClassName="active" exact>
+                HOME
+              </NavLink>
             </li>
             <li>
-              <a href="link">CATÁLOGO</a>
+              <NavLink to="/products" activeClassName="active">
+                CATÁLOGO
+              </NavLink>
             </li>
             <li>
-              <a href="link">ADMIN</a>
+              <NavLink to="/admin" activeClassName="active">
+                ADMIN
+              </NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;

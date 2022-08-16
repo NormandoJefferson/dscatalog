59afa91a291c133d48d9b07caf832e1cedd8745d
NormandoJefferson <normandojefferson@hotmail.com>
2022-08-16 18:47:59 -0300

    ProductCard

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,23 +1,42 @@
 import ProductCard from 'components/ProductCard';
+import { Product } from 'types/product';
 
 const Catalog = () => {
+
+  const product :Product = {
+    id: 2,
+    name: 'Smart TV',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Nisi quas eos beatae minima culpa perspiciatis',
+    price: 2190.0,
+    imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
+    date: '2020-07-14T10:00:00Z',
+    categories:[
+      {
+        id: 1,
+        name: 'Livros'
+      },
+      {
+        id: 3,
+        name: 'Computadores'
+      },
+    ],
+  };
+
   return (
     <div className="container my-4">
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quas eos beatae minima culpa perspiciatis dolores, vel earum temporibus possimus repellendus dicta. Officia dolores omnis at! Eligendi maxime placeat dolorum!
         </div>
       </div>
     </div>

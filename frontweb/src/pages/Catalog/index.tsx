dbbc3ff538f473e9ea5e97e40926be26dd55a54d
NormandoJefferson <normandojefferson@hotmail.com>
2022-08-18 17:17:28 -0300

    Catalog - ProductDetails links

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,16 +1,17 @@
 import ProductCard from 'components/ProductCard';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 
 const Catalog = () => {
 
-  const product :Product = {
+  const product: Product = {
     id: 2,
     name: 'Smart TV',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Nisi quas eos beatae minima culpa perspiciatis',
     price: 2190.0,
     imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
     date: '2020-07-14T10:00:00Z',
-    categories:[
+    categories: [
       {
         id: 1,
         name: 'Livros'
@@ -26,17 +27,26 @@ const Catalog = () => {
     <div className="container my-4">
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
+        
       </div>
     </div>
   );
--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,6 @@
-import {ReactComponent as ArrowIcon} from '../../assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice/index';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -8,11 +9,12 @@ const ProductDetails = () => {
 
         <div className='product-details-container'>
             <div className='base-card product-details-card'>
-                <div className='goback-container'>
-                    <ArrowIcon/>
-                    <h2>VOLTAR</h2>
-                </div>
-
+                <Link to="/products">
+                    <div className='goback-container'>
+                        <ArrowIcon />
+                        <h2>VOLTAR</h2>
+                    </div>
+                </Link>
                 <div className="row">
                     <div className="col-xl-6">
                         <div className='img-container'>
@@ -21,14 +23,14 @@ const ProductDetails = () => {
 
                         <div className='name-price-container'>
                             <h1>Nome do produto</h1>
-                            <ProductPrice price={2345.67}/>
+                            <ProductPrice price={2345.67} />
                         </div>
                     </div>
 
                     <div className="col-xl-6">
                         <div className='description-container'>
                             <h2>Descrição do produto</h2>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                                 Temporibus voluptate dignissimos nulla ad eligendi.</p>
                         </div>
                     </div>

2f43dfcae3366db4f5d337ffc774fb573ffea46d
NormandoJefferson <normandojefferson@hotmail.com>
2022-08-30 11:39:09 -0300

    Login card styling

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -11,30 +11,28 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private JwtTokenStore tokenStore;
-	
-	private static final String[] PUBLIC = {"/oauth/token"};
-	
-	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
-	
-	private static final String[] ADMIN = {"/users/**"};
-	
+
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+
+	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+
+	private static final String[] ADMIN = { "/users/**" };
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
 	}
-
+	
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-	
-		http.authorizeRequests() 
-		.antMatchers(PUBLIC).permitAll() 
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() 
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") 
-		.antMatchers(ADMIN).hasRole("ADMIN") 
-		.anyRequest().authenticated(); 
+		
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
+				.permitAll().antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN").antMatchers(ADMIN)
+				.hasRole("ADMIN").anyRequest().authenticated();
 	}
 }
